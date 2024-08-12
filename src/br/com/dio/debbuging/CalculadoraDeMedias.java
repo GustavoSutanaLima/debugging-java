9c7cdf7266613d8c42f17b06cdcf24d2373899af
Gustavo Sutana <gustavosutanalima@gmail.com>
2024-08-12 13:17:12 -0300

    Primeiro commit.: Adciona comentários de meu entendimento sobre o código e contúdo deste.

--- src/br/com/dio/debbuging/CalculadoraDeMedias.java
+++ src/br/com/dio/debbuging/CalculadoraDeMedias.java
@@ -1,21 +1,33 @@
 package br.com.dio.debbuging;
 
 import java.util.Scanner;
-
+        /*
+         * DICA.: Durante a DEPURAÇÃO (OU DEBUGGING) a opção Step into (setinha pra baixo) entra no método no qual 
+         * o programa está parado;
+         */
 public class CalculadoraDeMedias {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        //inicia o objeto Scanner (captura de informações pelo terminal)
+        Scanner scan = new Scanner(System.in); 
+        
+        //cria uma array de Strings.
+        //No nomento da criação desse array usa-se colchetes depois do tipo;
+        //Os elementos do array são separados por vírgula "," e ficam entre chaves;
         String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
 
+        //Outra maneira de declarar este mesmo array é assim.:
+        //String alunos[] = {"Camila", "Lucas", "Bruna", "Pedro"};
+ 
         double media = calculaMediaDaTurma(alunos, scan);
 
-        System.out.printf("Média da turma %.1f", media);
+        System.out.printf("Média da turma: %.1f", media);
     }
 
     public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
 
         double soma = 0;
-        for(String aluno : alunos) {
+        for(String aluno : alunos) { //laço for-each muito usado com arrays (para-cada elementos do array )
+                                     //nesse caso, a variável aluno assume acada um dos valores do arra;
             System.out.printf("Nota do aluno %s: ", aluno);
             double nota = scanner.nextDouble();
             soma += nota;
--- src/br/com/dio/debbuging/Main.java
+++ src/br/com/dio/debbuging/Main.java
@@ -2,12 +2,13 @@ package br.com.dio.debbuging;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Iniciou do programa no método main.");
+        System.out.println("Inicio do programa no método main.");
         a();
-        System.out.println("Finalizou do programa no método main.");
+        System.out.println("Fim do programa no método main.");
     }
 
-    static void a() {
+    static void a() { //detalhe.: Nessa linha, na declaração do método não foi utilizada a palavra chave
+                      //public, quando isso acontece o Java entente que o public é o padrão;
         System.out.println("Entrou no método a.");
         b();
         System.out.println("Finalizou o método a.");
@@ -15,14 +16,17 @@ public class Main {
 
     static void b() {
         System.out.println("Entrou no método b.");
-        for(int i = 0; i <= 4; i++) System.out.println(i);
+        for(int i = 0; i <= 4; i++) System.out.println(i); //for executado em uma única linha
         c();
         System.out.println("Finalizou o método b.");
     }
 
     static void c(){
         System.out.println("Entrou no método c.");
-        //Thread.dumpStack();
+        Thread.dumpStack(); //Imprime na tela o Stack Trace.: as threads que o código percorre durante a sua
+                            //executação: IMPORTANTÍSSIMO: A LEITURA DA STACK TRACE É FEITA **DE BAIXO PARA CIMA**
+                            //E SUA UTILIZAÇÃO É DE SUMA IMPORTANCIA NO DEBBUGING DO CÓDIGO;
+                            //TAMBÉM É UTILIZADA NO TRATAMENTO DE EXCEÇÕES;
         System.out.println("Finalizou o método c.");
     }
 
